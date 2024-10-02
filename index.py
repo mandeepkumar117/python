f10bf71ed9b974f987a5a62b98f092e052ab4cb2
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-10-02 15:25:41 +0530

    first day in pyton

---
+++ index.py
@@ -0,0 +1,83 @@
+# name=int(input("Enter first number: "))
+# name2=int(input("Enter second number:"))
+# name3=name**name2
+# print(name3)
+# num=int(input("Enter number:"))
+# if(num>0):
+#     print("number is greater thaan 0")
+# elif(num==0):
+#     print("number is Zero")    
+# else:
+#     print("number is less than 0")    
+
+
+# marks=int(input("enter your marks: "))
+# if(marks>=90):
+#     print("Grade = A")
+# elif(marks>=70):
+#     print("Grade = B ")
+# elif(marks>=50):
+#     print("Grade = C ")     
+# else:
+#     print("Faild")      
+
+
+
+# age=int(input("Enter age :"))
+# #num1=13
+# if(age<=12):
+#     print("He is child")
+# elif(age<=19):
+#     print("He is teenager")
+# elif(age<=65):
+#     print("He is adult")
+# elif(age>=65):
+#     print("He is senior")
+
+
+
+# age=int(input("Enter age"))
+# if(age<=18):
+#     print("He eligible for vote")
+# else:
+#     print("He not eligible for vote")
+
+
+
+# num=int(input("Enter any number:"))
+# if(num%2==0):
+#     print("number is even")
+# else:
+#     print("number is odd")
+
+
+
+char=input("Enter any charter")
+if(char=='A'):
+    print("A is vowel")
+elif(char=='E'):
+    print("E is vowel")
+elif(char=='I'):
+    print("I is vowel")
+elif(char=='O'):
+    print("O is vowel")
+elif(char=='U'):
+    print("U is vowel")
+else:
+    print("That is consunant")
+
+
+
+# num=int(input("Enter student marks:"))
+# if(num<=100 and num>=90):
+#     print("He got Grade = A")
+# elif(num<=89 and num>=80):
+#     print("He got Grade = B")
+# elif(num<=79 and num>=70):
+#     print("He got Grade = C")
+# elif(num<=69 and num>=60):
+#     print("He got Grade = D")
+# elif(num<60):
+#     print("He is Faild ⅩⅩⅩ")
+
+
