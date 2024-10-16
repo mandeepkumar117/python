5a681ed4554395eef8de5cc237adee2acfdf0b48
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-10-16 15:33:54 +0530

    add new topic on 16th october

--- tupple.py
+++ tupple.py
@@ -13,25 +13,57 @@
 
 
         # stack                       |                     heap(object)
-        #   Reference                 |
-        #   variable                  |                    "apple" adress is 736
+        #  (Reference)  variable      |
+        #                             |                    "apple" adress is 736
         #                             |                          
         #                             |                     20 addres is 080
         #   fruit addres is 656       |
-        #                             |                      100 addres os 640
+        #                             |                      100 addres is 640
         #                             |                  ______________ 
         #                             |                 |736| 080| 640|
         #                             |                 |___|____|____|
         #                             |               656
 
-fruit=("apple",20,100)
-print(type(fruit))
-# fruit[2]=200 #tuple immutable
-print(id(fruit))
-print(id(fruit[0]),id(fruit[1]),id(fruit[2]))
-fruit=("apple",20,100)+("banana",9999)
-print(fruit)
-fruit=("apple",20,99)
-print(fruit)
-# fruit=99999999
+# fruit=("apple",20,100)
+# print(type(fruit))
+# # fruit[2]=200 #tuple immutable
+# print(id(fruit))
+# print(id(fruit[0]),id(fruit[1]),id(fruit[2]))
+# fruit=("apple",20,100)+("banana",9999)
 # print(fruit)
+# fruit=("apple",20,99)
+# print(fruit)
+
+# number=(34,45)+(56,90)
+# number=number + (89,90)
+# print(number)
+
+# q)
+lis=[["ajay","mandeep"],(23,45)]
+# print(type(lis[1]))
+# lis[1][0]=89
+# print(lis)
+# lis.pop()
+# print(lis)
+
+# q.2)
+# t=(34,55,[3,4,555])
+# print(t)
+# t[2][1]=987
+# print(t)
+# t[2].append(456)
+# print(t)
+
+# q.3)
+# lis=[["ajay","aman"],(45,[87,67])]
+# lis[1][1][0]=56
+# print(lis)
+# lis[1][1].append(23)
+# print(lis)
+
+#len() #max(), #min(), #sum(), #index(), #count()
+t=(3,4,5,6,7,3)
+a=t.count(3)
+print(a)
+a=sorted(t)
+print(a)
