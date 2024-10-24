9fd731019322affa8131c307066495a086d8e203
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-10-24 15:37:17 +0530

    add new file and topic

---
+++ if.py
@@ -0,0 +1,9 @@
+# condditional statement(decision making)(if-else)
+# control flow statement(looping)
+
+
+age=int(input("enter age"))
+if age>=18:
+    print("you are eligible")
+else:
+    print("you are not eligible")    
--- set.py
+++ set.py
@@ -11,3 +11,24 @@
 
 # a u b u c={33}
 # a^ b ^ c={2}
+
+
+s1={1,2,3,4,3,9,11,17}
+s2={5,4,9,17,89}
+s3={"a,","b", 1,2,3,88}
+r=s1.difference(s2)
+# print(r)
+# r=s1-s2
+# print(r)
+# r=s2-s1
+# print(r)
+s1.union(s2)
+print(s1)
+s1.update(s2)
+print(s1)
+s1.add(344)
+print(s1)
+s1.add((2,90,99))
+print(s1)
+s1.add((2,90))
+print(s1)
