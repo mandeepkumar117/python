d5d6604483d4f60f125f561b827bd3e84c057233
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-11-15 15:30:23 +0530

    add new topic and logic

--- trianglepattern.py
+++ trianglepattern.py
@@ -1,9 +1,102 @@
-n=int(input("Enter row no:"))
-i=0
-while i<n:
+# n=int(input("Enter row no:"))
+# i=0
+# while i<n:
+# *****
+# *****
+# *****
+# *****
+# *****
     # print('*' * n)
-    # print('*' * i + (n-1) * ' ')    
+
+# *
+# **
+# ***
+# ****    
+    # print('*' * i + (n-1) * ' ')  
+ 
+# *****
+# ****
+# ***
+# **
+# * 
     # print((n-i) * '*' + ' ' * i)
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
     # print(' ' * i + '*' * (n-i))
-    print(' ' * (n-i) + ' *' * i)
-    i=i+1
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+    # print(' ' * (n-i) + ' *' * i)
+    # print(' ' * i + ' *' * (n-i))
+
+    # i=i+1
+
+
+# *
+# * *
+# * * *
+# * * * * 
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *    
+
+# Q.2
+
+# *    
+# **
+# ***
+# ****
+# ***
+# **
+# *
+
+# n=int(input("Enter number: "))
+# i=1
+# while i<n:
+#     print('*' * i + (n-1) * ' ')
+#     i=i+1
+# i=i-2
+# while i>0:
+#     print('*' * i + (n-1) * ' ')
+#     i=i-1
+
+
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+# n=int(input("Enter number: "))
+# i=1
+# while i<=n:
+#     print(' ' * (n-i) + '*' * i)
+#     i=i+1
+# i=i-2
+# while i>0:
+#     print((n-i) * ' ' + '*' * i)
+#     i=i-1
+
+
+n=int(input("Enter row no: "))
+i=n
+while i>0:
+    print('*' * i)
+    i=i-1
+i=i+2
+while i<=n:
+    print('*' * i)
+    i=i+1    
