0c7167bb419c6a4522f23f418bbc69b28bd24f52
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-12-09 15:30:43 +0530

    add ansyasic topic

---
+++ my_calculator_for_python_and_import_file.py
@@ -0,0 +1,6 @@
+x=10
+y=20
+def add_calculator(x,y):
+    return x+y
+def sub_calculator(x,y):
+    return x-y
---
+++ new.py
@@ -0,0 +1,6 @@
+import my_calculator_for_python_and_import_file as m
+from my_calculator_for_python_and_import_file import add_calculator as mandeep, sub_calculator as mandeep1
+print(m.x)
+print(m.y)
+print(mandeep(5,30))
+print(mandeep1(5,30))
